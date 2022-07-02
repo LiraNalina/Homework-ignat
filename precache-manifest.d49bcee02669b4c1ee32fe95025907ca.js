2153af78eafe77c7deb2f75f79360da09bda722a
unknown <mara3575@hotmail.com>
2022-07-02 16:52:39 +0600

    Updates

---
+++ precache-manifest.d49bcee02669b4c1ee32fe95025907ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54908a060c7fb30fe2406e151c7d3ff",
+    "url": "/Homework-ignat/index.html"
+  },
+  {
+    "revision": "72954ec05adb73363c31",
+    "url": "/Homework-ignat/static/css/main.06099583.chunk.css"
+  },
+  {
+    "revision": "99c28928ba051c44b4da",
+    "url": "/Homework-ignat/static/js/2.e87a72b2.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Homework-ignat/static/js/2.e87a72b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72954ec05adb73363c31",
+    "url": "/Homework-ignat/static/js/main.83ae4ee4.chunk.js"
+  },
+  {
+    "revision": "6572a7cecaee0fe91862",
+    "url": "/Homework-ignat/static/js/runtime-main.3e4e6958.js"
+  }
+]);
