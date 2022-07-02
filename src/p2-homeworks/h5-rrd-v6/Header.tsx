aed2a78aad74ff3b20b24f33ac9873456abf0aed
unknown <mara3575@hotmail.com>
2022-07-02 20:57:29 +0600

    Added NavLinks to Header

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {PATH} from "../h5/Routes";
+import s from "*.module.css";
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
         <div>
             // add NavLinks
-
+            <NavLink to={PATH.PRE_JUNIOR} className={s.link} >pre-junior</NavLink>
         </div>
     )
 }
--- src/p2-homeworks/h5-rrd-v6/Pages.tsx
+++ src/p2-homeworks/h5-rrd-v6/Pages.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+import PreJunior from "./pages/PreJunior";
+import Error404 from "./pages/Error404";
+
+import {Route, Routes, Navigate} from "react-router-dom";
+
+
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
     // add paths
@@ -9,18 +15,19 @@ function Pages() {
     return (
         <div>
             {/*Routes выбирает первый подходящий роут*/}
-            {/*<Routes>*/}
+            <Routes>
 
                 {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
-                {/*<Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>}/>*/}
+                <Route path={'/'} element={<Navigate to={PATH.PRE_JUNIOR}/>  }/>
 
-                {/*<Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>*/}
+                <Route path={PATH.PRE_JUNIOR} element={<PreJunior/>}/>
+
                 // add routes
 
                 {/*он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-                {/*<Route path={'/*'} element={<Error404/>}/>*/}
+                <Route path={'/*'} element={<Error404/>}/>
 
-            {/*</Routes>*/}
+            </Routes>
         </div>
     )
 }
